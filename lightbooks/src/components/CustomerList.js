53b9bae9d9d2e2f1af4d24ff86130d6e51a7fd64
Kevin Letchford <kevin@letchford.co>
2016-12-06 23:51:48 +0000

    multiline functions

--- lightbooks/src/components/CustomerList.js
+++ lightbooks/src/components/CustomerList.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 var CUSTOMERDATA = require('../../customers.json');
 
-
 class CustomerList extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -15,7 +13,12 @@ class CustomerList extends Component {
       <div className="CustomerList">
    <ul>
      {
-        this.state.data.map((item,i) => <li key={i}>{item.name.first} {item.name.last}</li>)
+        this.state.data.slice(0, 10).map((item,i) => (
+        <li key={i}>
+          <h2>{item.name.first} {item.name.last}</h2>
+          <div>{item.balance}</div>
+        </li>)
+        )
     }
    </ul>
       </div>
